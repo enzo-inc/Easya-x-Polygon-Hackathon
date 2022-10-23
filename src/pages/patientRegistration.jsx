47e1ecfbc257bb5f554d0a5fae1a6a2dc956bb5a
Arish Tripathi <arishrian123@gmail.com>
2022-10-23 10:49:35 +0100

    psuh new

--- src/pages/patientRegistration.jsx
+++ src/pages/patientRegistration.jsx
@@ -1,26 +1,9 @@
 import React from "react";
 import { CountryDropdown } from "react-country-region-selector";
 import PhoneInput from "react-phone-input-2";
-
 import { useFormik } from "formik";
-
 import './register.css'
 import TopBar from '../components/topBar/topBar';
-import styled from "styled-components";
-
-const Text = styled.text`
-    font-size: 3.5em;
-    text-align: left;
-    text-transform: uppercase;
-    letter-spacing: .08em;
-    color: #396afc;  /* fallback for old browsers */
-    color: -webkit-linear-gradient(to right, #2948ff, #396afc);  /* Chrome 10-25, Safari 5.1-6 */
-    color: linear-gradient(to right, #2948ff, #396afc); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    font-family: SpaceMonoRegular;
-    font-weight: 700;
-    opacity: 80%;
-`
-
 import styled from "styled-components";
 
 const Text = styled.text`
